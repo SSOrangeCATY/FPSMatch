04d79be13acc377c09df974fe73c9d27ef77eee0
SSOragneCATY <jumaoz@qq.com>
2024-11-16 17:56:55 +0800

    map update

--- src/main/java/com/phasetranscrystal/fpsmatch/core/data/BombAreaData.java
+++ src/main/java/com/phasetranscrystal/fpsmatch/core/data/BombAreaData.java
@@ -5,7 +5,6 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
