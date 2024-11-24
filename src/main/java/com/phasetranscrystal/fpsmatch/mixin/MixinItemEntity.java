a06cd5058096e59dc84ca971cee60db2543ed233
SSOragneCATY <jumaoz@qq.com>
2024-11-24 20:29:22 +0800

    BUG

--- src/main/java/com/phasetranscrystal/fpsmatch/mixin/MixinItemEntity.java
+++ src/main/java/com/phasetranscrystal/fpsmatch/mixin/MixinItemEntity.java
@@ -1,17 +1,13 @@
 package com.phasetranscrystal.fpsmatch.mixin;
 
-import com.phasetranscrystal.fpsmatch.FPSMatch;
 import com.phasetranscrystal.fpsmatch.core.BaseMap;
 import com.phasetranscrystal.fpsmatch.core.BaseTeam;
 import com.phasetranscrystal.fpsmatch.core.FPSMCore;
 import com.phasetranscrystal.fpsmatch.core.map.BlastModeMap;
 import com.phasetranscrystal.fpsmatch.item.CompositionC4;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.scores.Objective;
-import net.minecraft.world.scores.Scoreboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinItemEntity {
     @Shadow public abstract ItemStack getItem();
 
-    @Inject(at = {@At("HEAD")}, method = "playerTouch(Lnet/minecraft/world/entity/player/Player;)V", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "playerTouch", cancellable = true)
     public void fpsMatch$playerTouch$CustomC4(Player player, CallbackInfo ci) {
         if(!player.isCreative()){
             if(this.getItem().getItem() instanceof CompositionC4){
@@ -32,7 +28,7 @@ public abstract class MixinItemEntity {
                     return;
                 };
                 BaseTeam team = map.getMapTeams().getTeamByPlayer(player);
-                if(team != null && map instanceof BlastModeMap blastModeMap){
+                if(team != null && map instanceof BlastModeMap<?> blastModeMap){
                     if(!blastModeMap.checkCanPlacingBombs(team.getName())){
                         ci.cancel();
                         return;
