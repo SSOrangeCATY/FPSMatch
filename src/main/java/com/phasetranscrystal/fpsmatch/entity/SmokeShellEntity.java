8febc7762c36e9a93cc4c2874e0ba134df700c8d
SSOragneCATY <jumaoz@qq.com>
2024-12-31 17:19:08 +0800

    update

--- src/main/java/com/phasetranscrystal/fpsmatch/entity/SmokeShellEntity.java
+++ src/main/java/com/phasetranscrystal/fpsmatch/entity/SmokeShellEntity.java
@@ -6,6 +6,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
@@ -23,54 +26,95 @@ import net.minecraft.world.phys.Vec3;
 import java.util.Random;
 
 public class SmokeShellEntity extends ThrowableItemProjectile {
-    public final int lifeTick;
-    private int lifeLeft;
-    private int state;
-    private int particleTicker = 0;
-    public final int r;
+    private static final EntityDataAccessor<Integer> LIFE_LEFT = SynchedEntityData.defineId(SmokeShellEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> STATE = SynchedEntityData.defineId(SmokeShellEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> PARTICLE_TICKER = SynchedEntityData.defineId(SmokeShellEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> LIFE_TICK = SynchedEntityData.defineId(SmokeShellEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> R = SynchedEntityData.defineId(SmokeShellEntity.class, EntityDataSerializers.INT);
+
 
     public SmokeShellEntity(LivingEntity pShooter, Level pLevel, int lifeTick, int state, int r) {
         super(EntityRegister.SMOKE_SHELL.get(), pShooter, pLevel);
-        this.lifeTick = lifeTick;
-        this.lifeLeft = lifeTick;
-        this.state = state;
-        this.r = r;
+        this.setLifeTick(lifeTick);
+        this.setLifeLeft(lifeTick);;
+        this.setState(state);
+        this.setR(r);
     }
 
     public SmokeShellEntity(LivingEntity pShooter, Level pLevel){
         super(EntityRegister.SMOKE_SHELL.get(), pShooter, pLevel);
-        this.lifeTick = 300;
-        this.r = 2;
     }
 
     public SmokeShellEntity(EntityType<SmokeShellEntity> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
-        this.lifeTick = 300;
-        this.r = 2;
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        this.entityData.define(LIFE_TICK, 300);
+        this.entityData.define(R, 2);
+        this.entityData.define(LIFE_LEFT, 0);
+        this.entityData.define(STATE, 0);
+        this.entityData.define(PARTICLE_TICKER, 0);
+    }
+    public int getR(){
+        return this.entityData.get(R);
+    }
+    public void setR(int r){
+        this.entityData.set(R,r);
+    }
+    public int getLifeTick(){
+        return this.entityData.get(LIFE_TICK);
+    }
+
+    public void setLifeTick(int lifeTick){
+        this.entityData.set(LIFE_TICK,lifeTick);
+    }
+
+    public void setState(int state){
+        this.entityData.set(STATE,state);
+    }
+
+    public int getState(){
+        return this.entityData.get(STATE);
+    }
+
+    public int getLifeLeft(){
+        return this.entityData.get(LIFE_LEFT);
+    }
+
+    public void setLifeLeft(int lifeLeft){
+        this.entityData.set(LIFE_LEFT,lifeLeft);
+    }
+    public int getParticleTicker(){
+        return this.entityData.get(PARTICLE_TICKER);
+    }
+    public void setParticleTicker(int particleTicker){
+        this.entityData.set(PARTICLE_TICKER,particleTicker);
     }
 
     @Override
     public void tick() {
-        if (lifeLeft-- <= 0) {
+        if (this.getLifeLeft() <= 0) {
             this.discard();
             return;
-        } else if (lifeLeft < lifeTick / 2 && state == 0) {
-            this.state = 1;
+        } else if (this.getLifeLeft() < this.getLifeTick() / 2 && this.getState() == 0) {
+            this.setState(1);
         }
-        this.lifeLeft--;
+        this.setLifeLeft(getLifeLeft() - 1);
         //TODO particles generate
 
 
-        if (state == 1) {
-            if (particleTicker == 5) {
+        if (this.getState() == 1) {
+            if (this.getParticleTicker() == 5) {
                 this.level().addParticle(ParticleTypes.SMOKE, this.getX(), this.getY(), this.getZ(), 0.0D, 0.0D, 0.0D);
-                particleTicker = 0;
+                this.setParticleTicker(0);
             } else {
-                particleTicker++;
+                this.setParticleTicker(this.getParticleTicker() + 1);
             }
-        } else if (state == 2) {
+        } else if (this.getState() == 2) {
             Random random = new Random();
-            this.level().addParticle(ParticleTypes.SMOKE, this.getX() + random.nextFloat(r), this.getY() + random.nextFloat(r), this.getZ() + random.nextFloat(r), 0.0D, 0.0D, 0.0D);
+            this.level().addParticle(ParticleTypes.SMOKE, this.getX() + random.nextFloat(this.getR()), this.getY() + random.nextFloat(this.getR()), this.getZ() + random.nextFloat(this.getR()), 0.0D, 0.0D, 0.0D);
             this.level().addParticle(ParticleTypes.ASH, this.getX(), this.getY(), this.getZ(), random.nextFloat(-0.2F, 0.2F), random.nextFloat(0.3F), random.nextFloat(-0.2F, 0.2F));
         }
 
@@ -84,10 +128,10 @@ public class SmokeShellEntity extends ThrowableItemProjectile {
 
     @Override
     protected void onHit(HitResult r) {
-        if (state == 2) return;
+        if (this.getState() == 2) return;
         super.onHit(r);
         if (!(r instanceof BlockHitResult result)) return;
-        if (state == 0) state = 1;
+        if (this.getState() == 0) this.setState(1);
 
         if (result.getDirection().getAxis().isHorizontal()) {
             Vec3 delta = getDeltaMovement();
@@ -98,7 +142,7 @@ public class SmokeShellEntity extends ThrowableItemProjectile {
         } else {
             this.setDeltaMovement(0, 0, 0);
             this.setNoGravity(true);
-            this.state = 2;
+            this.setState(2);
         }
     }
 }
