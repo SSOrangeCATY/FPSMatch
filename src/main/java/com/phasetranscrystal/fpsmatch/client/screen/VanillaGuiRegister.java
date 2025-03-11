8db3ff0fad1c78656a817ee63ed398731950c1ca
mad_sky <lycdada@outlook.com>
2025-03-11 20:06:22 +0800

    shop editor initial

--- src/main/java/com/phasetranscrystal/fpsmatch/client/screen/VanillaGuiRegister.java
+++ src/main/java/com/phasetranscrystal/fpsmatch/client/screen/VanillaGuiRegister.java
@@ -3,10 +3,7 @@ package com.phasetranscrystal.fpsmatch.client.screen;
 import com.phasetranscrystal.fpsmatch.FPSMatch;
 
 
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.world.inventory.MenuType;
-import net.minecraft.world.item.ItemStack;
-
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,17 +14,10 @@ public class VanillaGuiRegister {
             DeferredRegister.create(ForgeRegistries.MENU_TYPES, FPSMatch.MODID);
     public static final RegistryObject<MenuType<EditorShopContainer>> EDITOR_SHOP_CONTAINER =
             CONTAINERS.register("editor_shop_menu",
-                    () -> IForgeMenuType.create((windowId, inv, buf) -> {
-                        ItemStack stack = buf.readItem(); // 读取 ItemStack
-                        return new EditorShopContainer(windowId, inv, stack);
-                    })
+                    () -> IForgeMenuType.create((windowId, inv, buf) ->
+                            new EditorShopContainer(windowId, inv, buf.readItem()))
             );
-
     public static final RegistryObject<MenuType<EditShopSlotMenu>> EDIT_SHOP_SLOT_MENU = CONTAINERS.register(
             "edit_shop_slot_menu", () -> IForgeMenuType.create(EditShopSlotMenu::new));
 
-    public static void register(){
-        MenuScreens.register(EDITOR_SHOP_CONTAINER.get(), EditorShopScreen::new);
-        MenuScreens.register(EDIT_SHOP_SLOT_MENU.get(), EditShopSlotScreen::new);
-    }
 }
